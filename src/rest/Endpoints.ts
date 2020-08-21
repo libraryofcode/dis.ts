3a56994075405bf52cc07b395580d6c7e1b94778
gavintjhxx <gavintjhx@gmail.com>
2020-08-21 18:07:19 +0800

    user endpoints

--- src/rest/Endpoints.ts
+++ src/rest/Endpoints.ts
@@ -29,15 +29,24 @@ export default class Endpoints {
    * - PUT
    * - DELETE
    */
-  public static CHANNEL_MESSAGE_USER_REACTION(channelID: string, messageID: string, reaction: string, userID: string) {
+  public static CHANNEL_MESSAGE_USER_REACTION(
+    channelID: string,
+    messageID: string,
+    reaction: string,
+    userID: string
+  ) {
     return `/channels/${channelID}/messages/${messageID}/reactions/${reaction}/${userID}`;
   }
   /**
-    * `/channels/${channelID}/messages/${messageID}/reactions/${reaction}`
-    * - GET
-    * - DELETE
-    */
-  public static CHANNEL_MESSAGE_REACTION(channelID: string, messageID: string, reaction: string) {
+   * `/channels/${channelID}/messages/${messageID}/reactions/${reaction}`
+   * - GET
+   * - DELETE
+   */
+  public static CHANNEL_MESSAGE_REACTION(
+    channelID: string,
+    messageID: string,
+    reaction: string
+  ) {
     return `/channels/${channelID}/messages/${messageID}/reactions/${reaction}`;
   }
   /**
@@ -45,7 +54,10 @@ export default class Endpoints {
    * - GET
    * - DELETE
    */
-  public static CHANNEL_MESSAGE_REACTIONS(channelID: string, messageID: string) {
+  public static CHANNEL_MESSAGE_REACTIONS(
+    channelID: string,
+    messageID: string
+  ) {
     return `/channels/${channelID}/messages/${messageID}/reactions`;
   }
   /**
@@ -56,41 +68,73 @@ export default class Endpoints {
     return `/channels/${channelID}/bulk-delete`;
   }
   /**
-    * `/channels/${channelID}/permissions/${overwriteID}`
-    * - PUT
-    * - DELETE
-    */
+   * `/channels/${channelID}/permissions/${overwriteID}`
+   * - PUT
+   * - DELETE
+   */
   public static CHANNEL_OVERWRITE_PERMISSIONS(channelID, overwriteID) {
     return `/channels/${channelID}/permissions/${overwriteID}`;
   }
   /**
-    * `/channels/${channelID}/invites`
-    * - GET
-    * - POST
-    */
+   * `/channels/${channelID}/invites`
+   * - GET
+   * - POST
+   */
   public static CHANNEL_INVITES(channelID: string) {
     return `/channels/${channelID}/invites`;
   }
   /**
-    * `/channels/${channelID}/typing`
-    * - POST
-    */
+   * `/channels/${channelID}/typing`
+   * - POST
+   */
   public static CHANNEL_TYPING(channelID: string) {
     return `/channels/${channelID}/typing`;
   }
   /**
-    * `/channels/${channelID}/pins`
-    * - GET
-    */
+   * `/channels/${channelID}/pins`
+   * - GET
+   */
   public static CHANNEL_PINNED_MESSAGES(channelID: string) {
     return `/channels/${channelID}/pins`;
   }
   /**
-    * `/channels/${channelID}/pins/${messageID}`
-    * - PUT
-    * - DELETE
-    */
+   * `/channels/${channelID}/pins/${messageID}`
+   * - PUT
+   * - DELETE
+   */
   public static CHANNEL_PINNED_MESSAGE(channelID: string, messageID: string) {
     return `/channels/${channelID}/pins/${messageID}`;
+  }
+
+  /**
+   * `/users/@me`
+   * - GET
+   * - PATCH
+   */
+  public static CURRENT_USER() {
+    return `/users/@me`;
+  }
+  /**
+   * `/users/${userID}`
+   * - GET
+   */
+  public static USER(userID: string) {
+    return `/users/${userID}`;
+  }
+  /**
+   * `/users/@me/guilds`
+   * - GET
+   * - PATCH
+   */
+  public static CURRENT_USER_GUILDS() {
+    return `/users/@me/guilds`;
+  }
+  /**
+   * ` /users/@me/channels`
+   * - POST
+   * - GET
+   */
+  public static CREATE_DM() {
+    return `/users/@me/channels`;
   }
 }
