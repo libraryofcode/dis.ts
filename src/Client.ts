ff6e0074c91eedcee9f96d5d689558092af9bf0e
DedShotTM <dedshottm@gmail.com>
2020-11-25 19:59:51 -0500

    bruh

--- src/Client.ts
+++ src/Client.ts
@@ -3,9 +3,7 @@ import Socket from './gateway/WebSocket';
 
 export const client = new EventEmitter();
 // eslint-disable-next-line comma-dangle
-// Socket('token',  intents number. you can try 512 (GUILD_MESSAGES) );
-
-Socket('NzY5MzUwODc1ODA1MTIyNjEw.X5Nv1g.DHFt0k3kmpB_det7Ir0-SPOyFxM', 512);
+// Socket('<token>', /* intents number. you can try 512 (GUILD_MESSAGES) */);
 
 //  client.on('eventName', data = {
 //    ...
