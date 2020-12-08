453781be6a6531e835aeb76233c689800c3c3ef8
Bsian <bsian@staff.libraryofcode.us>
2020-12-08 09:24:05 +0000

    Safer remaining calculation

--- src/rest/RESTBucket.ts
+++ src/rest/RESTBucket.ts
@@ -15,7 +15,11 @@ export default class RESTBucket {
   updateRatelimitInfo(header: any) {
     Object.keys(header).forEach((k) => (header[k] = isNaN(header[k]) ? header[k] : Number(header[k]))); // save convering each to number later
     if (header['x-ratelimit-limit'] && this.limit !== header['x-ratelimit-limit']) this.limit = header['x-ratelimit-limit'];
-    if (this.remaining !== header['x-ratelimit-remaining']) this.remaining = header['x-ratelimit-remaining'] === undefined ? 1 : header['x-ratelimit-remaining'];
+    if (header['x-ratelimit-remaining'] !== undefined) {
+      if (header['x-ratelimit-remaining'] < this.remaining) this.remaining = header['x-ratelimit-remaining'];
+    } else {
+      this.remaining = 1;
+    }
     if (header['x-ratelimit-reset-after'] !== undefined) this.resetAt = (header['x-ratelimit-reset-after'] * 1000) + Date.now();
   }
 
