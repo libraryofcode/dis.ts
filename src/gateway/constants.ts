5e16c48bd59a9a965ba2a474f7e133df7a0678d6
DedShotTM <36904983+DedShotTM@users.noreply.github.com>
2020-12-09 14:53:54 -0500

    Basic Websocket structure (#9)
    * [0]
    
    * .
    
    * Opcodes and Close codes from /src/util/Constants.ts
    
    * Uninstall node fetch
    
    * Inbuilt requester
    
    Co-authored-by: Bsian <bsian@staff.libraryofcode.us>

---
+++ src/gateway/constants.ts
@@ -0,0 +1,54 @@
+import { GATEWAY_OPCODES } from '../util/Constants';
+
+// https://discord.com/developers/docs/topics/gateway#commands-and-events-gateway-events
+export const EVENTS = {
+  READY: 'ready',
+  CHANNEL_CREATE: 'channelCreate',
+  CHANNEL_UPDATE: 'channelUpdate',
+  CHANNEL_DELETE: 'channelDelete',
+  CHANNEL_PINS_UPDATE: 'channelPinsUpdate',
+  GUILD_CREATE: 'guildCreate',
+  GUILD_UPDATE: 'guildUpdate',
+  GUILD_DELETE: 'guildDelete',
+  GUILD_BAN_ADD: 'guildBanAdd',
+  GUILD_BAN_REMOVE: 'guildBanRemove',
+  GUILD_EMOJIS_UPDATE: 'guildEmojiesUpdate',
+  GUILD_INTEGRATIONS_UPDATE: 'guildIntegrationsUpdate',
+  GUILD_MEMBER_ADD: 'guildMemberAdd',
+  GUILD_MEMBER_REMOVE: 'guildMemberRemove',
+  GUILD_MEMBER_UPDATE: 'guildMemberUpdate',
+  GUILD_MEMBERS_CHUNK: 'guildMembersChunk',
+  GUILD_ROLE_CREATE: 'guildRoleCreate',
+  GUILD_ROLE_UPDATE: 'guildRoleUpdate',
+  GUILD_ROLE_DELETE: 'guildRoleDelete',
+  INVITE_CREATE: 'inviteCreate',
+  INVITE_DELETE: 'inviteDelete',
+  MESSAGE_CREATE: 'messageCreate',
+  MESSAGE_UPDATE: 'messageUpdate',
+  MESSAGE_DELETE: 'messageDelete',
+  MESSAGE_DELETE_BULK: 'messageDeleteBulk',
+  MESSAGE_REACTION_ADD: 'messageReactionAdd',
+  MESSAGE_REACTION_REMOVE: 'messageReactionRemove',
+  MESSAGE_REACTION_REMOVE_ALL: 'messageReactionRemoveAll',
+  MESSAGE_REACTION_REMOVE_EMOJI: 'messageReactionRemoveEmoji',
+  PRESENCE_UPDATE: 'presenceUpdate',
+  TYPING_START: 'typingStart',
+  USER_UPDATE: 'userUpdate',
+  VOICE_STATE_UPDATE: 'voiceStateUpdate',
+  VOICE_SERVER_UPDATE: 'guildServerUpdate',
+  WEBHOOKS_UPDATE: 'webhooksUpdate',
+};
+
+// https://discord.com/developers/docs/topics/gateway#heartbeat
+export const Heartbeat = {
+  op: GATEWAY_OPCODES.HEARTBEAT,
+  d: null,
+};
+
+// https://discord.com/developers/docs/topics/gateway#payloads
+export interface Payload {
+  op: number;
+  s: number;
+  t: string;
+  d: any;
+}
