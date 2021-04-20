fe1e27f458322063852a74506cc16698851dd639
Matthew R <matthew@staff.libraryofcode.org>
2021-04-20 16:47:23 -0400

    Add Embed interface struct

--- structures/Embed.ts
+++ structures/Embed.ts
@@ -1,41 +1,50 @@
+export interface EmbedAuthor {
+  icon_url?: string;
+  name?: string;
+  proxy_icon_url?: string;
+  url?: string;
+}
+
+export interface EmbedFooter {
+  icon_url?: string;
+  text: string;
+}
+
+export interface EmbedField {
+  inline?: boolean;
+  name: string;
+  value: string;
+}
+
+export interface EmbedFooter {
+  icon_url?: string;
+  text: string;
+}
+
+export interface EmbedMedia {
+  height?: number;
+  proxy_url?: string;
+  url?: string;
+  width?: number;
+}
+
+export interface EmbedProvider {
+  name?: string;
+  url?: string;
+}
+
 export default interface Embed {
-  author?: {
-    icon_url?: string;
-    name?: string;
-    proxy_icon_url?: string;
-    url?: string;
-  };
+  author?: EmbedAuthor;
   color?: number;
   description?: string;
-  fields?: [ { inline?: boolean; name: string; value: string } ];
-  footer?: {
-    icon_url?: string;
-    text: string;
-  };
-  image?: {
-    height?: number;
-    proxy_url?: string;
-    url?: string;
-    width?: number;
-  };
-  provider?: {
-    name?: string;
-    url?: string;
-  };
-  thumbnail?: {
-    height?: number;
-    proxy_url?: string;
-    url?: string;
-    width?: number;
-  };
+  fields?: EmbedField[];
+  footer?: EmbedFooter;
+  image?: EmbedMedia;
+  provider?: EmbedProvider;
+  thumbnail?: EmbedMedia;
   timestamp?: Date;
   title?: string;
   type?: 'rich' | 'image' | 'video' | 'gifv' | 'article' | 'link';
   url?: string;
-  video?: {
-    height?: number;
-    proxy_url?: string;
-    url?: string;
-    width?: number;
-  };
+  video?: EmbedMedia;
 }
