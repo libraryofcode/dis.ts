d9bca81f6e3dd77c603e3fcfb8de1ee47ab2f6e5
Matthew R <matthew@staff.libraryofcode.org>
2021-04-20 16:37:29 -0400

    change Base abstract class to interface

--- structures/Base.ts
+++ structures/Base.ts
@@ -1,11 +1,4 @@
-export default abstract class Base {
-  public id: string;
-
-  constructor(id: string) {
-    this.id = id;
-  }
-
-  toString() {
-    return `[${this.constructor.name}${this.id ? ` ${this.id}` : ''}]`;
-  }
+export default interface Base {
+  id: string;
+  toString(): string;
 }
