58926b447be289ef0ad64bd4b420a1dc70dae9c7
Bsian <bsian@staff.libraryofcode.us>
2021-05-08 22:29:31 +0100

    feat(structures): GuildTemplate interface serialized_source_guild has a different Guild Partial than existing one

---
+++ structures/GuildTemplate.ts
@@ -0,0 +1,15 @@
+import Guild from './Guild';
+
+export default interface GuildTemplate {
+  code: string;
+  created_at: Date;
+  creator: unknown; // TODO User
+  creator_id: string;
+  description: string | null;
+  is_dirty: boolean | null;
+  name: string;
+  serialized_source_guild: Guild; // REVIEW This is partial but doesn't match GuildPartial interface
+  source_guild_id: string;
+  updated_at: Date;
+  usage_count: number;
+}
