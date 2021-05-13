a1bc74b41cb77e3d7085b70bc333d8eecdbc2f35
Bsian <bsian@staff.libraryofcode.us>
2021-05-13 12:11:23 +0100

    Export all structures

---
+++ structures/index.ts
@@ -0,0 +1,16 @@
+export { default as Base } from './Base';
+export { default as BotGateway } from './BotGateway';
+export { default as Channel, ChannelPartial, WebhookChannel, ChannelType, ThreadMember, ThreadMetadata, VideoQuality } from './Channel';
+export { default as Emoji, PartialEmoji } from './Emoji';
+export { default as Guild, InviteGuild, TemplateGuild, WebhookGuild, DefaultMessageNotificationLevel, ExplicitContentFilterLevel, GuildFeatures, MFALevel, PremiumTier, SystemChannelFlags, VerificationLevel, WelcomeScreen, WelcomeScreenChannel } from './Guild';
+export { default as GuildIntegration, BotIntegration, IntegrationAccount, IntegrationApplication, IntegrationExpireBehaviour, InterationType } from './GuildIntegration';
+export { default as GuildMember } from './GuildMember';
+export { default as GuildPreview } from './GuildPreview';
+export { default as GuildTemplate } from './GuildTemplate';
+export { default as Invite, InviteMetadata, InviteTargetType } from './Invite';
+export { default as Message, MessageActivity, MessageActivityType, MessageFlags, MessageReference, MessageType, Reaction } from './Message';
+export { default as Permissions } from './Permissions';
+export { default as UserConnection, ConnectionVisibilityType } from './UserConnections';
+export { default as VoiceRegion } from './VoiceRegion';
+export { default as VoiceState } from './VoiceState';
+export { default as Webhook, WebhookType } from './Webhook';
