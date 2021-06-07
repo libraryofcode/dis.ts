0f5618b079a54a5f2334e28b24555d399fc341f8
SiroTM <SiroTM>
2021-06-07 19:30:49 -0400

    feat(structures): Role interface

---
+++ structures/Role.ts
@@ -0,0 +1,18 @@
+import Base from './Base';
+
+export default interface Role extends Base {
+  color: number;
+  hoist: boolean;
+  managed: boolean;
+  mentionable: boolean;
+  name: string;
+  permissions: string;
+  position: number;
+  tags?: roleTags;
+}
+
+export interface roleTags {
+  bot_id?: string;
+  integration_id?: string;
+  premium_subscriber: null;
+}
