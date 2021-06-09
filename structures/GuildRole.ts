ce06dfbfdff20e0d12917c7394f6bdcda2a34bb2
SiroTM <SiroTM>
2021-06-09 09:36:32 -0400

    rename Role to GuildRole

--- structures/GuildRole.ts
+++ structures/GuildRole.ts
@@ -1,17 +1,18 @@
 import Base from './Base';
+import Permissions from './Permissions';
 
-export default interface Role extends Base {
+export default interface GuildRole extends Base {
   color: number;
   hoist: boolean;
   managed: boolean;
   mentionable: boolean;
   name: string;
-  permissions: string;
+  permissions: Permissions;
   position: number;
-  tags?: roleTags;
+  tags?: RoleTags;
 }
 
-export interface roleTags {
+export interface RoleTags {
   bot_id?: string;
   integration_id?: string;
   premium_subscriber?: null;
