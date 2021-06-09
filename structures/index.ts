a725c635c8f77d0fae3759428ada0fa484b60d35
SiroTM <SiroTM>
2021-06-09 09:58:55 -0400

    refactor(Structures): GuildIntegration

--- structures/index.ts
+++ structures/index.ts
@@ -3,7 +3,7 @@ export { default as BotGateway } from './BotGateway';
 export { default as Channel, ChannelPartial, WebhookChannel, ChannelType, ThreadMember, ThreadMetadata, VideoQuality } from './Channel';
 export { default as Emoji, PartialEmoji } from './Emoji';
 export { default as Guild, InviteGuild, TemplateGuild, WebhookGuild, DefaultMessageNotificationLevel, ExplicitContentFilterLevel, GuildFeatures, MFALevel, PremiumTier, SystemChannelFlags, VerificationLevel, WelcomeScreen, WelcomeScreenChannel } from './Guild';
-export { default as GuildIntegration, BotIntegration, IntegrationAccount, IntegrationApplication, IntegrationExpireBehaviour, InterationType } from './GuildIntegration';
+export { default as GuildIntegration, BotIntegration, IntegrationAccount, IntegrationApplication, IntegrationExpireBehavior, InterationType } from './GuildIntegration';
 export { default as GuildMember } from './GuildMember';
 export { default as GuildPreview } from './GuildPreview';
 export { default as GuildTemplate } from './GuildTemplate';
