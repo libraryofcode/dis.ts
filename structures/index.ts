9ab44d85f93ae297e38f281b6407bc7d532e5528
SiroTM <SiroTM>
2021-06-09 10:24:53 -0400

    move PermissionOverwrite in index.ts

--- structures/index.ts
+++ structures/index.ts
@@ -1,5 +1,5 @@
 export { default as Base } from './Base';
-export { default as Channel, ChannelPartial, WebhookChannel, ChannelType, ThreadMember, ThreadMetadata, VideoQuality, PermissionOverwrite } from './Channel';
+export { default as Channel, ChannelPartial, WebhookChannel, ChannelType, ThreadMember, ThreadMetadata, VideoQuality } from './Channel';
 export { default as BotGateway } from './Gateway';
 export { default as Emoji, PartialEmoji } from './Emoji';
 export { default as Guild, InviteGuild, TemplateGuild, WebhookGuild, DefaultMessageNotificationLevel, ExplicitContentFilterLevel, GuildFeatures, MFALevel, PremiumTier, SystemChannelFlags, VerificationLevel, WelcomeScreen, WelcomeScreenChannel } from './Guild';
@@ -9,7 +9,7 @@ export { default as GuildPreview } from './GuildPreview';
 export { default as GuildTemplate } from './GuildTemplate';
 export { default as Invite, InviteMetadata, InviteTargetType } from './Invite';
 export { default as Message, MessageActivity, MessageActivityType, MessageFlags, MessageReference, MessageType, Reaction } from './Message';
-export { default as Permissions } from './Permissions';
+export { default as Permissions, PermissionOverwrite } from './Permissions';
 export { default as UserConnection, ConnectionVisibilityType } from './UserConnections';
 export { default as VoiceRegion } from './VoiceRegion';
 export { default as VoiceState } from './VoiceState';
