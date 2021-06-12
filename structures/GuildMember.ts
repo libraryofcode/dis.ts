fcea6cc58adac4ae92729bb697e9d92e3ec04b60
SiroTM <80137236+SiroTM@users.noreply.github.com>
2021-06-12 17:26:01 -0400

    fix(structures): GuildMember prop types (#38)

--- structures/GuildMember.ts
+++ structures/GuildMember.ts
@@ -1,11 +1,13 @@
+import Permissions from './Permissions';
+
 export default interface GuildMember {
   deaf: boolean;
   joined_at: Date;
   mute: boolean;
   nick?: string | null;
   pending?: boolean;
-  permissions?: unknown; // TODO BigInt permissions;
-  premium_since: Date | null;
+  permissions?: Permissions;
+  premium_since?: Date | null;
   roles: string[];
   user?: unknown; // TODO User;
 }
